44dfefdce56613613dd932e1106f2f0b13d0e74e
k-tobe <wxfyq156@yahoo.co.jp>
2018-04-16 21:24:01 +0900

    Initial Commit

---
+++ git_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git_test
+//
+//  Created by 戸部功治郎 on 2018/04/16.
+//  Copyright © 2018年 戸部功治郎. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
