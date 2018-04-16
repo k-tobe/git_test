27b13e4188fa893d80b74ddfeeeb6206192a6610
k-tobe <wxfyq156@yahoo.co.jp>
2018-04-16 21:35:20 +0900

    test edit

--- git_test/main.cpp
+++ git_test/main.cpp
@@ -1,15 +1,38 @@
-//
-//  main.cpp
-//  git_test
-//
-//  Created by 戸部功治郎 on 2018/04/16.
-//  Copyright © 2018年 戸部功治郎. All rights reserved.
-//
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//　メイン関数
+int main(void){
+    // 入力格納用
+    int parm = 0;
+    
+    // 高さを標準入力より指定する
+    scanf("%d",&parm);
+    
+    if(parm%2==0) {
+        // 偶数の場合「invalid」を出力
+        printf("invalid");
+    } else {
+        int turnPoint = parm/2;
+        int endPoint = parm-1;
+        int subNum=1;
+        
+        for(int i=0; i<parm; i++){
+            char lineData[parm];
+            memset(&lineData, '.', parm);
+            lineData[endPoint] = 'j';
+            lineData[parm] = '\0';
+            
+            if (i == parm-1) {
+                memset(lineData, 'j', sizeof(lineData));
+            } else if(i > turnPoint) {
+                lineData[0] = 'j';
+                subNum++;
+            }
+            printf("%s\n", lineData);
+        }
+    }
     return 0;
 }
